b3f52179eba45387201994c23ef1496696560c52
haseeb-developer <haseeb.devv@gmail.com>
2025-02-05 19:12:36 +0500

    Updated

---
+++ src/components/detectPlace.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const detectPlace = () => {
+
+  return (
+    <div className="detect-country">
+      <h2>Detect Country/Place</h2>
+    </div>
+  );
+};
+
+export default detectPlace;
